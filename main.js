1b3f86e5c0a592b818d83d25ac8e17b6454bfa30
professorsch <101644066+professorsch@users.noreply.github.com>
2023-09-09 15:50:17 -0300

    Atualizando para lista 5 - funcionalidades do teclado.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } 
+    else {
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -13,6 +20,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     tecla.onclick = function() {
         tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (evento) {
+       
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+         tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
     }
 
 }
